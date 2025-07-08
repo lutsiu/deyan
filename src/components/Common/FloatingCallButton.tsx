bef43452eb9a2b9ef88f0270131db2705fce4d19
lutsiu <oleksandr.lutsiuk.1603@gmail.com>
2025-07-08 17:23:28 +0200

    Call Us Floating Button is fully responsive

--- src/components/Common/FloatingCallButton.tsx
+++ src/components/Common/FloatingCallButton.tsx
@@ -11,8 +11,10 @@ export default function FloatingCallButton() {
   return (
     <button
       className="
-        fixed bottom-[2rem] right-[8.8rem] z-50 
-        w-[9.6rem] h-[9.6rem]
+        fixed bottom-[2rem] z-50 
+        right-[2.4rem] md:right-[4rem] lg:right-[5rem] xl:right-[7rem] 2xl:right-[8.8rem]
+        w-[8rem] h-[8rem] 
+        md:w-[9.6rem] md:h-[9.6rem]
         rounded-full 
         flex items-center justify-center
         cursor-pointer"
@@ -22,9 +24,7 @@ export default function FloatingCallButton() {
       <div className="absolute inset-0 bg-green-500 rounded-full opacity-70" />
       <Icon
         icon="mdi-light:phone"
-        width={38}
-        height={38}
-        className="relative z-10 text-beige-200"
+        className="relative z-10 text-beige-200 w-[4rem] h-[4rem] md:w-[3.8rem] md:h-[3.8rem]"
       />
     </button>
   );
